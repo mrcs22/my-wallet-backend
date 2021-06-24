ebf6032f16eeaf20ec8ebd1be38ccfd7b9f1d89a
mrcs22 <marcusmoraes2010@hotmail.com>
2021-06-24 00:23:36 -0300

    finishing endpoint to sign in

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import Joi from "joi";
 import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
 import connection from "./database/database.js";
 
 const app = express();
@@ -51,6 +52,64 @@ app.post("/sign-up", async (req, res) => {
     );
 
     res.sendStatus(201);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+//############## Sign In ##################
+
+app.post("/sign-in", async (req, res) => {
+  try {
+    const userSchema = Joi.object({
+      email: Joi.string()
+        .email({ tlds: { allow: false } })
+        .required(),
+      password: Joi.string().required(),
+    });
+
+    const validationError = userSchema.validate(req.body).error;
+
+    if (validationError) {
+      const errorMessage = validationError.details[0].message;
+
+      res.status(400);
+      return res.send(errorMessage);
+    }
+
+    const { email, password } = req.body;
+
+    const result = await connection.query(
+      `
+    SELECT * FROM users
+    WHERE email LIKE $1
+    `,
+      [email]
+    );
+
+    const user = result.rows[0];
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.sendStatus(400);
+    }
+
+    const token = uuid();
+
+    await connection.query(
+      `
+    INSERT INTO sessions
+    (user_id, token)
+    VALUES
+    ($1,$2)
+    `,
+      [user.id, token]
+    );
+
+    delete user.password;
+    user.token = token;
+
+    res.send(user);
   } catch (err) {
     console.log(err);
     res.sendStatus(500);
